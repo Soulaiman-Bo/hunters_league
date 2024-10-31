6184cf1dbc0691a3f5b828e3cc6e0c4eb918a5cb
Soulaiman Bouhlal <bouhlal.soulaiman@gmail.com>
2024-10-31 22:52:18 +0100

    i added Species not found exception

--- src/main/java/com/javangers/hunters_league/web/errors/GlobalExceptionHandler.java
+++ src/main/java/com/javangers/hunters_league/web/errors/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.javangers.hunters_league.web.errors;
 
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -39,6 +40,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 
+    @ExceptionHandler(SpeciesAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleUserAlreadyExistsException(SpeciesAlreadyExistsException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
     @ExceptionHandler(InvalidLicenseException.class)
     public ResponseEntity<Map<String, String>> handleInvalidLicense(InvalidLicenseException ex) {
         return ResponseEntity.badRequest().body(Map.of("error", ex.getMessage()));
@@ -49,6 +57,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleAuthentication(EntityNotFoundException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 
 }
