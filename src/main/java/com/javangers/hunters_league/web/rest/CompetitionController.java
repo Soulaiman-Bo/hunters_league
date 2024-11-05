5a7c3a66618c5c62d4a36e9a2933b0f0dc577ff7
Soulaiman Bouhlal <bouhlal.soulaiman@gmail.com>
2024-11-05 13:44:26 +0100

    i added controller for creating a participation

--- src/main/java/com/javangers/hunters_league/web/rest/CompetitionController.java
+++ src/main/java/com/javangers/hunters_league/web/rest/CompetitionController.java
@@ -1,8 +1,11 @@
 package com.javangers.hunters_league.web.rest;
 
 import com.javangers.hunters_league.domain.Competition;
+import com.javangers.hunters_league.domain.Participation;
 import com.javangers.hunters_league.service.CompetitionService;
+import com.javangers.hunters_league.service.ParticipationService;
 import com.javangers.hunters_league.web.vm.CompetitionRequestVM;
+import com.javangers.hunters_league.web.vm.ParticipationRequestVM;
 import com.javangers.hunters_league.web.vm.mapper.CompetitionMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +22,8 @@ public class CompetitionController {
 
     private final CompetitionService competitionService;
     private final CompetitionMapper competitionMapper;
+    private final ParticipationService participationService;
+
 
     @PostMapping
     public ResponseEntity<Competition> CreateSpecies(@Valid @RequestBody CompetitionRequestVM request) {
@@ -34,6 +39,19 @@ public class CompetitionController {
             @PathVariable UUID competitionId) {
         Competition competition = competitionService.getCompetition(competitionId);
         return ResponseEntity.ok(competition);
+    }
+
+    @PostMapping("/{competitionId}/register")
+    public ResponseEntity<Participation> registerForCompetition(
+            @PathVariable UUID competitionId,
+            @Valid @RequestBody ParticipationRequestVM request) {
+
+        Participation participation = participationService.registerForCompetition(
+                competitionId,
+                request.getUserId()
+        );
+
+        return ResponseEntity.ok(participation);
     }
 
 }
