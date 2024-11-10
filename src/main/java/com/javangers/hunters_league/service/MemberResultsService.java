9fc7d9386d03d485083234e36f9997f3575d3a65
Soulaiman Bouhlal <bouhlal.soulaiman@gmail.com>
2024-11-10 14:50:42 +0100

    i added result service for getting a members history

---
+++ src/main/java/com/javangers/hunters_league/service/MemberResultsService.java
@@ -0,0 +1,11 @@
+package com.javangers.hunters_league.service;
+
+import com.javangers.hunters_league.service.dto.MemberResultDTO;
+import com.javangers.hunters_league.web.vm.MemberResultsFilterDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.UUID;
+
+public interface MemberResultsService {
+    Page<MemberResultDTO> getMemberResults(UUID userId, MemberResultsFilterDTO filterDTO);
+}
